389774c505135bfbbc8a7b9f7ea06d1605d44e90
soft_code <emmanuel.erhabor98@gmail.com>
2023-08-09 05:54:01 +0100

    coding go

--- main.go
+++ main.go
@@ -5,7 +5,26 @@ import (
 )
 
 func main() {
-	penniesPerText := 2.0
+	age := 20
+	yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental := yearsUntilEvent(age)
 
-	fmt.Printf("The type of penniesPerText is %T\n", penniesPerText)
+	fmt.Printf("mano firstNames are %v, %v, %v", yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental)
+
+}
+
+// Naked Return
+func yearsUntilEvent(age int) (yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental int) {
+	yearsUntilAdult = 18 - age
+	if yearsUntilAdult < 0 {
+		yearsUntilAdult = 0
+	}
+	yearsUntilDrinking = 21 - age
+	if yearsUntilDrinking < 0 {
+		yearsUntilDrinking = 0
+	}
+	yearsUntilCarRental = 25 - age
+	if yearsUntilCarRental < 0 {
+		yearsUntilCarRental = 0
+	}
+	return
 }
