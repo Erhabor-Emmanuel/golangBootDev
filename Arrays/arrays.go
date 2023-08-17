e6bdafa76ef11c7c02c34fba1b125c4e69a399a7
soft_code <emmanuel.erhabor98@gmail.com>
2023-08-17 03:52:00 +0100

    arrays in Go

--- Arrays/arrays.go
+++ Arrays/arrays.go
@@ -1,9 +1,23 @@
 package Arrays
 
+import (
+	"fmt"
+)
+
 func getMessageWithRetries() [6]string {
 	return [6]string{
 		"Click here to sign up",
 		"Pretty please click here",
 		"We beg you to sign up",
+	}
+}
+
+func send(name string, doneAt int) {
+	fmt.Printf("sending to %v....", name)
+	fmt.Println()
+
+	messages := getMessageWithRetries()
+	for i := 0; i < len(messages); i++ {
+
 	}
 }
