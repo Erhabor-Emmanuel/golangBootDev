8a61c936a6f9b3a92141ebdc800ce970f272acec
soft_code <emmanuel.erhabor98@gmail.com>
2023-08-17 01:39:21 +0100

    coding go

--- errorHandling/loops/loops.go
+++ errorHandling/loops/loops.go
@@ -1,9 +1,28 @@
 package loops
 
+import "fmt"
+
 func bulkSend(numMessages int) float64 {
 	totalCost := 0.0
 	for i := 0; i < numMessages; i++ {
 		totalCost += 1.0 + (0.01 * float64(i))
 	}
 	return totalCost
+}
+
+func test(numMessages int) {
+	fmt.Printf("Sending %v messages\n", numMessages)
+	cost := bulkSend(numMessages)
+	fmt.Printf("Bulk send complete! Cost = %.2f\n", cost)
+	fmt.Println("================================")
+}
+
+func maxMessages(thresh float64) int {
+	totalCost := 0.0
+	for i := 0; ; i++ {
+		totalCost += 1.0 + (0.01 * float64(i))
+		if totalCost > thresh {
+			return i
+		}
+	}
 }
