e4473d92582a2b2b4679252606e384df868e1c21
soft_code <emmanuel.erhabor98@gmail.com>
2023-08-17 03:48:18 +0100

    arrays in Go

--- Arrays/arrays.go
+++ Arrays/arrays.go
@@ -2,6 +2,8 @@ package Arrays
 
 func getMessageWithRetries() [6]string {
 	return [6]string{
-		"click here to sign up",
+		"Click here to sign up",
+		"Pretty please click here",
+		"We beg you to sign up",
 	}
 }
