11096609c789409e346be24107d10234e0f2100e
soft_code <emmanuel.erhabor98@gmail.com>
2023-08-17 03:11:32 +0100

    loops in Go

--- errorHandling/loops/whileLoop.go
+++ errorHandling/loops/whileLoop.go
@@ -13,20 +13,36 @@ func getMaxMessagesToSend(costMultiplier float64, maxCostInPennies int) int {
 }
 
 func fizzbuzz() {
-	for i := 0; i <= 100; i++ {
+	for i := 1; i <= 100; i++ {
 
 		if i%3 == 0 {
 			fmt.Println("numbers: fizz")
-		}
-		if i%5 == 0 {
+		} else if i%5 == 0 {
 			fmt.Println("numbers: buzz")
-		}
-		if i%3 == 0 && i%5 == 0 {
+		} else if i%3 == 0 && i%5 == 0 {
 			fmt.Println("numbers: fizzbuzz")
 		} else {
 			fmt.Printf("numbers: %v\n", i)
 		}
 	}
+}
+
+func printPrimes(max int) {
+	for n := 2; n < max+1; n++ {
+		if n == 2 {
+			fmt.Println(n)
+			continue
+		}
+		if n%2 == 0 {
+			continue
+		}
+	}
+}
+
+func testPrime(max int) {
+	fmt.Printf("Primes up to %v:\n", max)
+	printPrimes(max)
+	fmt.Println("===========================")
 }
 
 func main() {
