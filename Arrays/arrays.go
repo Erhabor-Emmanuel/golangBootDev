4f11ffd1f2012e7cbd7496c6f6c5ccf8f8a6485e
soft_code <emmanuel.erhabor98@gmail.com>
2023-08-17 04:12:28 +0100

    arrays in Go

--- Arrays/arrays.go
+++ Arrays/arrays.go
@@ -18,6 +18,15 @@ func send(name string, doneAt int) {
 
 	messages := getMessageWithRetries()
 	for i := 0; i < len(messages); i++ {
-
+		msg := messages[i]
+		fmt.Printf("Sending: %v", msg)
+		fmt.Println()
+		if i == doneAt {
+			fmt.Println("they responded!")
+			break
+		}
+		if i == len(messages)-1 {
+			fmt.Println("complete failure")
+		}
 	}
 }
