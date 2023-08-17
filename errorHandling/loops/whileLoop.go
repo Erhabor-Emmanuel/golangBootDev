fc29d101ccf0cc21edfc45378e88978e793a6984
soft_code <emmanuel.erhabor98@gmail.com>
2023-08-17 02:04:39 +0100

    coding go

--- errorHandling/loops/whileLoop.go
+++ errorHandling/loops/whileLoop.go
@@ -1,1 +1,10 @@
 package loops
+
+func getMaxMessagesToSend(costMultiplier float64, maxCostPennies int) int {
+	actualCostInPennies := 1.0
+	maxMessagesToSend := 0
+	for {
+		maxMessagesToSend++
+		actualCostInPennies *= costMultiplier
+	}
+}
